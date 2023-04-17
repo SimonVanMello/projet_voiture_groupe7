ed5e5bfc2500463cef7c010c6e7cd4142e2fef0d
desmazer123 <121011945+desmazer123@users.noreply.github.com>
2023-04-17 15:38:33 +0200

    test current

--- testcapteurCourant.py
+++ testcapteurCourant.py
@@ -1,6 +1,7 @@
 import board
 import busio
 import adafruit_ina219
+import time
 
 # Initialisation de la communication I2C avec le capteur INA219
 i2c = busio.I2C(11, 12)
@@ -11,4 +12,4 @@ while true:
     current_mA = ina219.current
 
     print("Courant : %.2f mA" % current_mA)
-    sleep(1)
+    time.sleep(1)
