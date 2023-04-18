dfdca95a5a7e99349296015ab5dd5bc135f08c1b
ShiryoMath <mathler.student@gmail.com>
2023-04-18 11:35:43 +0200

    modif vitesse

--- dev/Vitesse.py
+++ dev/Vitesse.py
@@ -5,7 +5,7 @@ import time
 pin1 = 11
 pin2 = 12
 en = 5
-temp1=1
+etat=1
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin1,GPIO.OUT)
@@ -26,52 +26,45 @@ while(1):
     
     if x=='r':
         print("run")
-        if(temp1==1):
+        if(etat==1):
          GPIO.output(pin1,GPIO.HIGH)
          GPIO.output(pin2,GPIO.LOW)
          print("Avancer")
-         x='z'
         else:
          GPIO.output(pin1,GPIO.LOW)
          GPIO.output(pin2,GPIO.HIGH)
          print("Reculer")
-         x='z'
+
 
 
     elif x=='s':
         print("La voiture s'arrête")
         GPIO.output(pin1,GPIO.LOW)
         GPIO.output(pin2,GPIO.LOW)
-        x='z'
 
     elif x=='f':
         print("La voiture avance")
         GPIO.output(pin1,GPIO.HIGH)
         GPIO.output(pin2,GPIO.LOW)
-        temp1=1
-        x='z'
+        etat=1
 
     elif x=='b':
         print("La voiture recule")
         GPIO.output(pin1,GPIO.LOW)
         GPIO.output(pin2,GPIO.HIGH)
         temp1=0
-        x='z'
 
     elif x=='l':
         print("Vitesse faible")
         p.ChangeDutyCycle(25)
-        x='z'
 
     elif x=='m':
         print("Vitesse normale")
         p.ChangeDutyCycle(50)
-        x='z'
 
     elif x=='h':
         print("Vitesse élevée")
         p.ChangeDutyCycle(75)
-        x='z'
      
     
     elif x=='e':
@@ -80,4 +73,4 @@ while(1):
         break
     
     else:
-        print("<<<  Wrong data, try again  >>>")
+        print("<<<  Veuillez entrer une des valeurs données  >>>")
