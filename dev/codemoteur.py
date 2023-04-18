684f23051f2be1716e7e0c308865fe238272560d
desmazer123 <121011945+desmazer123@users.noreply.github.com>
2023-04-18 13:32:35 +0200

    Update codemoteur.py

--- dev/codemoteur.py
+++ dev/codemoteur.py
@@ -1,34 +1,68 @@
+from time import sleep
 import RPi.GPIO as GPIO
-import time
-import Adafruit_PCA9685
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(27, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
-pwm = Adafruit_PCA9685.PCA9685()
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
 
-def setMotor(mode):
-    GPIO.output(27, mode)
-    GPIO.output(15, not  mode)
-    pwm.set_pwm(4,1024,3072)
-    pwm.set_pwm_freq(1000)
-    GPIO.output(17, mode)
-    GPIO.output(18, not  mode)
-    pwm.set_pwm(5,1024,3072)
+# Modifiez pour mettre les pins sur lesquels sont branchés les entrées de la L293D
+MOTOR1_EN = 4
+MOTOR1_A = 11
+MOTOR1_B = 12
 
+MOTOR2_EN = 5
+MOTOR2_A = 15
+MOTOR2_B = 13
 
-i=0
 try:
-        while i<4:
-                setMotor(GPIO.HIGH)
-                time.sleep(5)
-                setMotor(GPIO.LOW)
-                time.sleep(5)
-                i=i+1
-finally:
-        GPIO.output(27,GPIO.LOW)
-        GPIO.output(15,GPIO.LOW)
-        GPIO.output(17,GPIO.LOW)
-        GPIO.cleanup(18,GPIO.LOW)
+
+    # Configure les pins
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(MOTOR1_EN, GPIO.OUT)
+    GPIO.setup(MOTOR1_A, GPIO.OUT)
+    GPIO.setup(MOTOR1_B, GPIO.OUT)
+
+    GPIO.setup(MOTOR2_EN, GPIO.OUT)
+    GPIO.setup(MOTOR2_A, GPIO.OUT)
+    GPIO.setup(MOTOR2_B, GPIO.OUT)
+
+    motor1GPIO = GPIO.PWM(MOTOR1_EN, 100)
+    motor2GPIO = GPIO.PWM(MOTOR2_EN, 100)
+
+    # AVANCE
+
+    # Fais avancer le robot lentement (50%) en ligne droite
+    motor1GPIO.start(50)
+    GPIO.output(MOTOR1_A, GPIO.HIGH)
+    GPIO.output(MOTOR1_B, GPIO.LOW)
+
+    motor2GPIO.start(50)
+    GPIO.output(MOTOR2_A, GPIO.HIGH)
+    GPIO.output(MOTOR2_B, GPIO.LOW)
+
+    # Continu d'avancer pendant une seconde et demi
+    sleep(1.5)
+
+    # RECULE EN TOURNANT
+
+    # Fais reculer le robot en dessinant une courbe
+    motor1GPIO.start(100)
+    GPIO.output(MOTOR1_A, GPIO.LOW)
+    GPIO.output(MOTOR1_B, GPIO.HIGH)
+
+    motor2GPIO.start(60)
+    GPIO.output(MOTOR2_A, GPIO.LOW)
+    GPIO.output(MOTOR2_B, GPIO.HIGH)
+
+    # Continu de reculer pendant 2 secondes
+    sleep(2)
+
+    # Arrêt des moteurs en arrêtant les cycles de travail
+    motor1GPIO.stop()
+    motor2GPIO.stop()
+
+
+except KeyboardInterrupt:
+    pass
+except:
+    GPIO.cleanup()
+    raise
+
+GPIO.cleanup()
