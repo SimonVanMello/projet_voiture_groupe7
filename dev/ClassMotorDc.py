c11c808036bf2e5ac2104eab09ebc0cf5d1bc06b
desmazer123 <121011945+desmazer123@users.noreply.github.com>
2023-04-18 10:52:54 +0200

    version2

--- dev/ClassMotorDc.py
+++ dev/ClassMotorDc.py
@@ -1,68 +1,80 @@
-from time import sleep
+
 import RPi.GPIO as GPIO
+from time import sleep
 
-# Modifiez pour mettre les pins sur lesquels sont branchés les entrées de la L293D
-MOTOR1_EN = 4
-MOTOR1_A = 15
-MOTOR1_B = 13
 
-MOTOR2_EN = 5
-MOTOR2_A = 12
-MOTOR2_B = 11
+# Definition des pins
+M1_En = 4
+M1_In1 = 15
+M1_In2 = 13
 
-try:
+M2_En = 5
+M2_In1 = 12
+M2_In2 = 11
 
-    # Configure les pins
-    GPIO.setmode(GPIO.BCM)
 
-    GPIO.setup(MOTOR1_EN, GPIO.OUT)
-    GPIO.setup(MOTOR1_A, GPIO.OUT)
-    GPIO.setup(MOTOR1_B, GPIO.OUT)
+# Creation d'une liste des pins pour chaque moteur pour compacter la suite du code
+Pins = [[M1_En, M1_In1, M1_In2], [M2_En, M2_In1, M2_In2]]
 
-    GPIO.setup(MOTOR2_EN, GPIO.OUT)
-    GPIO.setup(MOTOR2_A, GPIO.OUT)
-    GPIO.setup(MOTOR2_B, GPIO.OUT)
 
-    motor1GPIO = GPIO.PWM(MOTOR1_EN, 100)
-    motor2GPIO = GPIO.PWM(MOTOR2_EN, 100)
+# Setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-    # AVANCE
+GPIO.setup(M1_En, GPIO.OUT)
+GPIO.setup(M1_In1, GPIO.OUT)
+GPIO.setup(M1_In2, GPIO.OUT)
 
-    # Fais avancer le robot lentement (50%) en ligne droite
-    motor1GPIO.start(50)
-    GPIO.output(MOTOR1_A, GPIO.HIGH)
-    GPIO.output(MOTOR1_B, GPIO.LOW)
+GPIO.setup(M2_En, GPIO.OUT)
+GPIO.setup(M2_In1, GPIO.OUT)
+GPIO.setup(M2_In2, GPIO.OUT)
 
-    motor2GPIO.start(50)
-    GPIO.output(MOTOR2_A, GPIO.HIGH)
-    GPIO.output(MOTOR2_B, GPIO.LOW)
 
-    # Continu d'avancer pendant une seconde et demi
-    sleep(1.5)
+# Voir aide dans le tuto
+M1_Vitesse = GPIO.PWM(M1_En, 100)
+M2_Vitesse = GPIO.PWM(M2_En, 100)
+M1_Vitesse.start(100)
+M2_Vitesse.start(100)
 
-    # RECULE EN TOURNANT
 
-    # Fais reculer le robot en dessinant une courbe
-    motor1GPIO.start(100)
-    GPIO.output(MOTOR1_A, GPIO.LOW)
-    GPIO.output(MOTOR1_B, GPIO.HIGH)
+def sens1(moteurNum) :
+    GPIO.output(Pins[moteurNum - 1][1], GPIO.HIGH)
+    GPIO.output(Pins[moteurNum - 1][2], GPIO.LOW)
+    print("Moteur", moteurNum, "tourne dans le sens 1.")
 
-    motor2GPIO.start(60)
-    GPIO.output(MOTOR2_A, GPIO.LOW)
-    GPIO.output(MOTOR2_B, GPIO.HIGH)
 
-    # Continu de reculer pendant 2 secondes
+def sens2(moteurNum) :
+    GPIO.output(Pins[moteurNum - 1][1], GPIO.LOW)
+    GPIO.output(Pins[moteurNum - 1][2], GPIO.HIGH)
+    print("Moteur", moteurNum, "tourne dans le sens 2.")
+
+def arret(moteurNum) :
+    GPIO.output(Pins[moteurNum - 1][1], GPIO.LOW)
+    GPIO.output(Pins[moteurNum - 1][2], GPIO.LOW)
+    print("Moteur", moteurNum, "arret.")
+
+def arretComplet() :
+    GPIO.output(Pins[0][1], GPIO.LOW)
+    GPIO.output(Pins[0][2], GPIO.LOW)
+    GPIO.output(Pins[1][1], GPIO.LOW)
+    GPIO.output(Pins[1][2], GPIO.LOW)
+    print("Moteurs arretes.")
+arretComplet()
+
+
+while True :
+    # Exemple de motif de boucle
+    sens1(1)
+    sleep(3)
+    sens1(2)
+    sleep(3)
+    arretComplet()
+    sleep(5)
+    sens2(1)
     sleep(2)
-
-    # Arrêt des moteurs en arrêtant les cycles de travail
-    motor1GPIO.stop()
-    motor2GPIO.stop()
-
-
-except KeyboardInterrupt:
-    pass
-except:
-    GPIO.cleanup()
-    raise
-
-GPIO.cleanup()
+    arret(1)
+    sleep(1)
+    sens2(2)
+    sleep(2)
+    arret(2)
+    sleep(1)
