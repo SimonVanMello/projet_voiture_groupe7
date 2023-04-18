941f1de537cc24b5351d75b741c20a1aaad13c37
ShiryoMath <mathler.student@gmail.com>
2023-04-18 12:08:22 +0200

    modif vitesse encore ptn

--- dev/Vitesse.py
+++ dev/Vitesse.py
@@ -4,18 +4,28 @@ import time
 
 pin1 = 11
 pin2 = 12
+pin3 = 15
+pin4 = 13
 en = 5
+en2 = 4
 etat=1
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin1,GPIO.OUT)
 GPIO.setup(pin2,GPIO.OUT)
 GPIO.setup(en,GPIO.OUT)
+GPIO.setup(pin3,GPIO.OUT)
+GPIO.setup(pin4,GPIO.OUT)
+GPIO.setup(en2,GPIO.OUT)
 GPIO.output(pin1,GPIO.LOW)
 GPIO.output(pin2,GPIO.LOW)
-p=GPIO.PWM(en,1000)
+GPIO.output(pin3,GPIO.LOW)
+GPIO.output(pin4,GPIO.LOW)
+p = GPIO.PWM(en,50)
+p2 = GPIO.PWM(en2,50)
 
 p.start(30)
+p2.start(30)
 print("\n")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
 print("\n")    
@@ -29,10 +39,14 @@ while(1):
         if(etat==1):
          GPIO.output(pin1,GPIO.HIGH)
          GPIO.output(pin2,GPIO.LOW)
+         GPIO.output(pin3,GPIO.HIGH)
+         GPIO.output(pin4,GPIO.LOW)
          print("Avancer")
         else:
          GPIO.output(pin1,GPIO.LOW)
          GPIO.output(pin2,GPIO.HIGH)
+         GPIO.output(pin3,GPIO.LOW)
+         GPIO.output(pin4,GPIO.HIGH)
          print("Reculer")
 
 
@@ -41,30 +55,39 @@ while(1):
         print("La voiture s'arrête")
         GPIO.output(pin1,GPIO.LOW)
         GPIO.output(pin2,GPIO.LOW)
+        GPIO.output(pin3,GPIO.LOW)
+        GPIO.output(pin4,GPIO.LOW)
 
     elif x=='f':
         print("La voiture avance")
         GPIO.output(pin1,GPIO.HIGH)
         GPIO.output(pin2,GPIO.LOW)
+        GPIO.output(pin3,GPIO.HIGH)
+        GPIO.output(pin4,GPIO.LOW)
         etat=1
 
     elif x=='b':
         print("La voiture recule")
         GPIO.output(pin1,GPIO.LOW)
         GPIO.output(pin2,GPIO.HIGH)
+        GPIO.output(pin3,GPIO.LOW)
+        GPIO.output(pin4,GPIO.HIGH)
         temp1=0
 
     elif x=='l':
         print("Vitesse faible")
         p.ChangeDutyCycle(25)
+        p2.ChangeDutyCycle(25)
 
     elif x=='m':
         print("Vitesse normale")
         p.ChangeDutyCycle(50)
+        p2.ChangeDutyCycle(50)
 
     elif x=='h':
         print("Vitesse élevée")
         p.ChangeDutyCycle(75)
+        p2.ChangeDutyCycle(75)
      
     
     elif x=='e':
