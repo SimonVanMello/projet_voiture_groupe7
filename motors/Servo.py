ff0f99ebdf686819ef094cad6091d609b2a96145
TropicoDebug <simon.vanmello@proton.me>
2023-04-18 06:08:08 +0200

    Servo class accessors

--- motors/Servo.py
+++ motors/Servo.py
@@ -7,10 +7,11 @@ from time import sleep
 
 class Servo:
     def __init__(self):
-        self.pwm = Adafruit_PCA9685.PCA9685()
-        self.pwm.set_pwm_freq(60)
-        self.SERVO_MIN = 150
-        self.SERVO_MAX = 600
+        self.__pwm = Adafruit_PCA9685.PCA9685()
+        self.__pwm.set_pwm_freq(60)
+        self.__position  = 375
+        self.__SERVO_MIN = 150
+        self.__SERVO_MAX = 600
 
     # def set_servo_pulse(self, channel ,pulse):
     #     pulse_length = 1000000
@@ -22,6 +23,20 @@ class Servo:
     #     pulse //= pulse_length
     #     pwm.set_pwm(channel, 0, pulse)
 
-    def move(self, position: int):
-        if position in range(self.SERVO_MIN, self.SERVO_MAX):
-            self.pwm.set_pwm(0, 0, position)
+    # getter function using property decorator
+    @property
+    def position(self) -> int:
+        return self.__position
+
+    @position.setter
+    def position(self, newPosition: int):
+        if newPosition in range(self.__SERVO_MIN, self.__SERVO_MAX):
+            # whenever the position change with a valid value, we update
+            # the actual position by calling self.__move()
+            self.__position = newPosition
+            self.__move()
+        else:
+            print("Invalid value")
+
+    def __move(self):
+        self.__pwm.set_pwm(0, 0, self.__position)
--- test_Servo.py
+++ test_Servo.py
@@ -6,8 +6,10 @@ from random import randint
 from time import sleep
 
 servo = Servo()
+# test the motor with 10 random positions
 for i in range(1, 11):
     position = randint(150, 650)
     print(f"Position {i}: {position}")
-    servo.move(position)
+    # call the setter which will call the move method
+    servo.position = position
     sleep(2)
