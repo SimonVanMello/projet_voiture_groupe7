d0abb78ae029bc91ae3ad46b894aadd46694fedb
TropicoDebug <simon.vanmello@proton.me>
2023-04-18 10:20:15 +0200

    test dc

--- motors/Dc.py
+++ motors/Dc.py
@@ -2,17 +2,34 @@
 #coding: utf-8
 
 import RPi.GPIO as GPIO
+import Adafruit_PCA9685
 from time import sleep
 
 
 class Dc:
-    def __init__(self, pinEnable: int, pinIn1: int, pinIn):
+    def __init__(self, pinEnable: int, pinIn1: int, pinIn2: int):
+        self.pinEnable = pinEnable
+        self.pinIn1 = pinIn1
+        self.pinIn2 = pinIn2
+
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pinIn1, GPIO.OUT)
+        GPIO.setup(self.pinIn2, GPIO.OUT)
+
         self.__pwm = Adafruit_PCA9685.PCA9685()
         self.__pwm.set_pwm_freq(60)
-        self.__position  = 200
+        self.speed = 200
+        self.__pwm.set_pwm(self.pinEnable, 0, self.speed)
 
     def move(self):
-        self.__pwm.set_pwm(4, 0, self.__position)
+        GPIO.output(self.pinIn1, GPIO.LOW)
+        GPIO.output(self.pinIn2, GPIO.HIGH)
+        
+    def stop(self):
+        GPIO.output(self.pinIn1, GPIO.LOW)
+        GPIO.output(self.pinIn2, GPIO.LOW)
 
-dc = Dc()
+dc = Dc(4, 15, 13)
 dc.move()
+sleep(4)
+dc.stop()
