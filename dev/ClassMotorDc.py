ff749bdc956a79d35b772765a7fca2ae588a0057
desmazer123 <121011945+desmazer123@users.noreply.github.com>
2023-04-18 09:14:21 +0200

    new motor

--- dev/ClassMotorDc.py
+++ dev/ClassMotorDc.py
@@ -1,21 +1,20 @@
 class Motor:
-    def __init__(self, pin1, pin2):
-        self.pin1 = pin1
-        self.pin2 = pin2
-        GPIO.setup(self.pin1, GPIO.OUT)
-        GPIO.setup(self.pin2, GPIO.OUT)
+    def __init__(self, pwm, in1, in2):
+        self.in1 = in1
+        self.in2 = in2
+        self.pwm = pwm
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
     
     def forward(self, speed):
-        GPIO.output(self.pin1, GPIO.HIGH)
-        GPIO.output(self.pin2, GPIO.LOW)
-        self.pwm = GPIO.PWM(self.pin1, 100)
-        self.pwm.start(speed)
+        GPIO.output(self.in1, GPIO.HIGH)
+        GPIO.output(self.in2, GPIO.LOW)
+        self.pwm.ChangeDutyCycle(speed)
     
     def backward(self, speed):
-        GPIO.output(self.pin1, GPIO.LOW)
-        GPIO.output(self.pin2, GPIO.HIGH)
-        self.pwm = GPIO.PWM(self.pin2, 100)
-        self.pwm.start(speed)
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.HIGH)
+        self.pwm.ChangeDutyCycle(speed)
     
     def stop(self):
-        self.pwm.stop()
+        self.pwm.ChangeDutyCycle(0)
