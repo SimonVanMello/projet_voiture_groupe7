cd630ecffdc39ec4c67273502561bcbd7c3819cc
TropicoDebug <simon.vanmello@proton.me>
2023-04-19 18:54:57 +0200

    fix ulrasonic unit test

--- unit_tests.py
+++ unit_tests.py
@@ -10,41 +10,40 @@ class Testultrasons(unittest.TestCase):
     def test_left_mesure(self):
         leftSensor = Ultrasonic(23,21)
         distance = leftSensor.getDistance()
-        self.assertEqual(distance, 20)
+        # self.assertEqual(distance, 20)
         borninf = self.distance + (self.distance/ 11.25)
         bornsup = self.distance - (self.distance/ 11.25)
-
-        if borninf <= self.distance <= bornsup:
-            print("Distance correct")
-        else:
-            print("Distance erronée")
+        self.assertTrue(orninf <= distance <= bornsup)
+        # if borninf <= self.distance <= bornsup:
+        #     print("Distance correct")
+        # else:
+        #     print("Distance erronée")
 
     def test_front_mesure(self):
         frontSensor = Ultrasonic(31,29)
         distance = frontSensor.getDistance()
-        self.assertEqual(distance, 20)
+        # self.assertEqual(distance, 20)
         borninf = self.distance + (self.distance/ 11.25)
         bornsup = self.distance - (self.distance/ 11.25)
+        self.assertTrue(orninf <= distance <= bornsup)
+        # if borninf <= self.distance <= bornsup:        
+        #     print("Distance correct")
 
-        if borninf <= self.distance <= bornsup:        
-            print("Distance correct")
-
-        else:
-            print("Distance erronée")
+        # else:
+        #     print("Distance erronée")
 
     def test_right_mesure(self):
         rightSensor = Ultrasonic(37,35)
         distance = rightSensor.getDistance()
-        self.assertEqual(distance, 20)
+        # self.assertEqual(distance, 20)
         borninf = self.distance + (self.distance/ 11.25)
         bornsup = self.distance - (self.distance/ 11.25)
-
-        if borninf <= self.distance <= bornsup:
-            print("Distance correct")
+        self.assertTrue(orninf <= distance <= bornsup)
+        # if borninf <= self.distance <= bornsup:
+        #     print("Distance correct")
             
-        else:
-            print("Distance erronée")
+        # else:
+        #     print("Distance erronée")
 
 if __name__ == '__main__':
-    #unittest.main()
-    pass
+    unittest.main()
