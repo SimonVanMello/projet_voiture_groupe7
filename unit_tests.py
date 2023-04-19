4584ed08160c8d1bee751dfc8d8e21a874b94db2
TropicoDebug <simon.vanmello@proton.me>
2023-04-19 22:08:28 +0200

    unittest

--- unit_tests.py
+++ unit_tests.py
@@ -7,12 +7,16 @@ import time
 from sensors.Ultrasonic import *
 
 class Testultrasonic(unittest.TestCase):
+    def __init__(self):
+        super.__init__()
+        self.distance = 10
+
     def test_left_mesure(self):
         leftSensor = Ultrasonic(23,21)
         distance = leftSensor.getDistance()
         # self.assertEqual(distance, 20)
-        borninf = self.distance + (self.distance/ 11.25)
-        bornsup = self.distance - (self.distance/ 11.25)
+        borninf = self.distance - (self.distance/ 11.25)
+        bornsup = self.distance + (self.distance/ 11.25)
         self.assertTrue(borninf <= distance <= bornsup)
         # if borninf <= self.distance <= bornsup:
         #     print("Distance correct")
@@ -23,8 +27,8 @@ class Testultrasonic(unittest.TestCase):
         frontSensor = Ultrasonic(31,29)
         distance = frontSensor.getDistance()
         # self.assertEqual(distance, 20)
-        borninf = self.distance + (self.distance/ 11.25)
-        bornsup = self.distance - (self.distance/ 11.25)
+        borninf = self.distance - (self.distance/ 11.25)
+        bornsup = self.distance + (self.distance/ 11.25)
         self.assertTrue(borninf <= distance <= bornsup)
         # if borninf <= self.distance <= bornsup:        
         #     print("Distance correct")
@@ -36,8 +40,8 @@ class Testultrasonic(unittest.TestCase):
         rightSensor = Ultrasonic(37,35)
         distance = rightSensor.getDistance()
         # self.assertEqual(distance, 20)
-        borninf = self.distance + (self.distance/ 11.25)
-        bornsup = self.distance - (self.distance/ 11.25)
+        borninf = self.distance - (self.distance/ 11.25)
+        bornsup = self.distance + (self.distance/ 11.25)
         self.assertTrue(borninf <= distance <= bornsup)
         # if borninf <= self.distance <= bornsup:
         #     print("Distance correct")
