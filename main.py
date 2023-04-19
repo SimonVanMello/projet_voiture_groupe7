be1348c2d283f0e2c830f7f70da936841efe3cdd
TropicoDebug <simon.vanmello@proton.me>
2023-04-19 13:59:16 +0200

    fix Dc

--- main.py
+++ main.py
@@ -7,25 +7,13 @@ import threading
 
 def circle():
     servo = SensorAndMotor()
-    servo.position = 300
     dc = Dc()
     dc.setup()
-    dc.setSpeed(20)
-    threadServo = threading.Thread(target=servo.run)
-    threadServo.start()
+    dc.setSpeed(40)
+    
     try:
-        while True:
-            inp = input("1: change speed\n2: change angle\n> ")
-            if inp == "stop":
-                dc.stop()
-                servo.position = 350
-                break
-            elif inp == "1":
-                inp = int(input("New speed: "))
-                dc.setSpeed(inp)
-            elif inp == "2":
-                inp = int(input("New angle: "))
-                servo.position = inp
+        servo.position = 275
+
     except:
         dc.stop()
         servo.position = 350
