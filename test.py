40a36711824a5d45a7124b0ec518ca73108bc413
ShiryoMath <mathler.student@gmail.com>
2023-04-20 12:43:11 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import unittest
+import time
+from sensors.Ultrasonic import Ultrasonic
+from sensors.Infra import Infra
+
+
+
+class TestInfra(unittest.TestCase):
+    def test_infra(self):
+        ir = Infra(20)
+        result = ir.getInfo()
+        self.assertEquals(1, result)
+
+if __name__ == '__main__':
+    unittest.main()
