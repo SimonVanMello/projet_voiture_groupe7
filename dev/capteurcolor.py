c81c437cb4a6157ad39e1db205cd7c8360a96014
ShiryoMath <mathler.student@gmail.com>
2023-04-21 08:40:16 +0200

    Update capteurcolor.py

--- dev/capteurcolor.py
+++ dev/capteurcolor.py
@@ -41,8 +41,40 @@ while True:
     print("Rouge : {}, Vert : {}, Bleu : {}".format(r, g, b))
     print("Température de couleur : {} K".format(c))
     time.sleep(1)
-
 '''
+
+
+''' a tester
+import time
+from machine import I2C
+from tcs34725 import TCS34725
+
+i2c = I2C(2)
+
+sensor = TCS34725(i2c)
+sensor.integration_time = 200 # Valeur plus grande = plus d'information collectée
+
+def corr_255(self):
+    # Appliquer une correction gamma sur une valeur entre 0 - 255
+    self.x /= 255
+    self.x = pow(self.x, 2.5)
+    self.self.x *= 255
+    return int(self.x) if self.x < 255 else 255
+
+def color(self):
+    # corrAppliquer la correction gamma à un tuple de couleur rgb
+    return corr_255(self.color[0]), corr_255(self.color[1]), corr_255(self.color[2])
+
+
+while True:
+    # Lecture de la couleur sur le capteur
+    rgb = sensor.color_rgb_bytes    # color_rgb_bytes
+    color_rgb = color( rgb )  # Appliquer correction Gamma
+    print( "rgb : %s   color_rgb : %s" % (rgb, color_rgb) )
+    time.sleep(1)
+'''
+
+
 
 ''' EN COURS
 
@@ -57,5 +89,4 @@ sensor = adafruit_tcs34725.TCS34725(i2c)
 print('Color: ({0}, {1}, {2})'.format(*sensor.color_rgb_bytes))
 print('Temperature: {0}K'.format(sensor.color_temperature))
 print('Lux: {0}'.format(sensor.lux))
-
 '''
