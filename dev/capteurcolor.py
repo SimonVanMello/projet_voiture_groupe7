df7a3bface3a10c34309ad8e3de3726d14c78231
ShiryoMath <mathler.student@gmail.com>
2023-04-21 09:46:20 +0200

    Update capteurcolor.py

--- dev/capteurcolor.py
+++ dev/capteurcolor.py
@@ -1,10 +1,9 @@
 import board
 import adafruit_tcs34725
+
 i2c = board.I2C()
+
 sensor = adafruit_tcs34725.TCS34725(i2c)
-
-
-
 
 print('Color: ({0}, {1}, {2})'.format(*sensor.color_rgb_bytes))
 print('Temperature: {0}K'.format(sensor.color_temperature))
